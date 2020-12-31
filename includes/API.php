1f5465be4dc54308216d30b39af4b1809b6125b4
func <funcer@outlook.com>
2020-12-31 22:12:00 +0800

    Strip the div wrapper natively

--- includes/API.php
+++ includes/API.php
@@ -180,14 +180,6 @@ class API extends \ApiBase {
 		$this->getResult()->addValue(null, $this->getModuleName(), $obj);
 	}
 
-	public static function stripWrapper( $html ) {
-		$m = [];
-		if ( preg_match( '/^<div class="mw-parser-output">(.*)<\/div>$/sU', $html, $m ) ) {
-			$html = $m[1];
-		}
-		return $html;
-	}
-
 	public function execute() {
 		$main = $this->getMain();
 		$action = $main->getVal('type');
@@ -363,7 +355,9 @@ class API extends \ApiBase {
 				$parser = MediaWikiServices::getInstance()->getParserFactory()->create();
 
 				// Set options for parsing
-				$opt = new \ParserOptions($user);
+				$opt = \ParserOptions::newCanonical($user);
+				$opt->setWrapOutputClass(''); // Set empty class name to avoid div warpper
+				$opt->enableLimitReport(false);
 
 				$text = $parser->preSaveTransform($text, $title, $user, $opt);
 				$output = $parser->parse($text, $title, $opt);
@@ -376,8 +370,7 @@ class API extends \ApiBase {
 				unset($opt);
 				unset($output);
 
-				// Useless p wrapper
-				$text = self::stripWrapper($text);
+				// Strip p wrapper
 				$text = \Parser::stripOuterParagraph($text);
 				$text = SpamFilter::sanitize($text);
 
